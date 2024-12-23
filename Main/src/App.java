1f9da12f4f9de636ffa431422f953b478f170851
Yi-Yongmin <lym8467@naver.com>
2024-12-23 23:53:54 +0900

    백준12789 계속 16%에서 실패

--- Main/src/App.java
+++ Main/src/App.java
@@ -5,50 +5,44 @@ public class App {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String K = br.readLine();
-        Set<Character> setLeft = new HashSet<Character>();
-        Set<Character> setRight = new HashSet<Character>();
-
-        setRight.add(']');
-        setLeft.add('[');
-        setLeft.add('(');
-        setRight.add(')');
-        while (!K.equals(".")) {
-            // no일경우에는 1 , yes 일 경우에는 0 인 변수 하나 설정.
-            int ans = 0;
-            // 스택 생성
-            Stack<Character> stack = new Stack<>();
-            // 문장 읽기
-            String str = K;
-            // 문장을 순회하면서 좌측 괄호의 경우에는 스택에 넣고 , 우측 괄호의 경우에는 pop해서 바로 비교
-            for (int i = 0; i < str.length() - 1; i++) {
-                if (setLeft.contains(str.charAt(i))) {// 좌측 괄호를 만나면 스택에 푸쉬
-                    stack.push(str.charAt(i));
-                } else if (setRight.contains(str.charAt(i)) && !stack.isEmpty()) {// 우측 괄호를 만나면 스택에서 바로 pop한뒤에 비교
-                    // pop을 위해서는 stack이 비어있어서는 안된다.
-                    Character left = stack.pop();
-                    Character right = str.charAt(i);
-                    if (left.equals('(') && right.equals(')'))
-                        continue;
-                    else if (left.equals('[') && right.equals(']'))
-                        continue;
-                    else {
-                        System.out.println("no");
-                        ans = 1;
-                        break;
-                    }
-                } else if (setRight.contains(str.charAt(i)) && stack.isEmpty()) { // 우측 괄호를 만났지만 , 스택이 비어있는 경우
-                    System.out.println("no");
-                    ans = 1;
-                    break;
-                }
-            }
-            if (ans == 0 && stack.isEmpty()) {
-                System.out.println("yes");
-            } else if (ans == 0 && !stack.isEmpty()) {
-                System.out.println("no");
-            }
-            K = br.readLine();
+        // N명이라는 입력 받기
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+        // N명 숫자 입력
+        for (int i = 0; i < N; i++) {
+            arr.add(Integer.parseInt(st.nextToken()));
         }
+
+        // 옆줄로 빠질 Stack
+        Stack<Integer> stack = new Stack<>();
+
+        // ans = 1일때 nice 출력
+        int ans = 1;
+
+        // 현재 순서가 N이 아니면 계속 돌기
+        for (int i = 0; i < N; i++) {
+            if (arr.get(i) == i + 1) {
+                continue;
+            }
+            if (stack.size() != 0) {
+                // 1. 스택이 존재하고 , top이 cnt 면 pop
+                if (stack.peek() == i + 1) {
+                    stack.pop();
+                } else {// 2. 스택이 존재하고 , top이 cnt 아니면
+                    if (stack.peek() < arr.get(i)) {
+                        System.out.println("Sad");
+                        ans = 0;
+                        break;
+                    } else {
+                        stack.push(i + 1);
+                    }
+                }
+            } else {// 스택이 존재하지 않고 , 줄 첫번째도 cnt가 아니니 푸쉬
+                stack.push(arr.get(i));
+            }
+        }
+        if (ans == 1)
+            System.out.println("Nice");
     }
 }
