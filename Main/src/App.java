10adfba6261a37ef1ce580f3aa43fd547e6e8dfd
Yi-Yongmin <lym8467@naver.com>
2024-12-27 23:50:10 +0900

    241227

--- Main/src/App.java
+++ Main/src/App.java
@@ -6,32 +6,13 @@ public class App {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // N개의 명령어
         int N = Integer.parseInt(br.readLine());
-        Deque<Integer> arr = new LinkedList<>();
-        // 큐에 숫자
+        String str = br.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
+        LinkedList<Integer> arr = new LinkedList<>();
         for (int i = 0; i < N; i++) {
-            String[] instruction = br.readLine().split(" ");
-            if (instruction.length == 2 && instruction[0].equals("1")) {
-                arr.offerFirst(Integer.parseInt(instruction[1]));
-            } else if (instruction.length == 2 && instruction[0].equals("2")) {
-                arr.offerLast(Integer.parseInt(instruction[1]));
-            } else if (instruction[0].equals("3")) {
-                int ans = arr.size() != 0 ? arr.poll() : -1;
-                System.out.println(ans);
-            } else if (instruction[0].equals("4")) {
-                int ans = arr.size() != 0 ? arr.pollLast() : -1;
-                System.out.println(ans);
-            } else if (instruction[0].equals("5")) {
-                System.out.println(arr.size());
-            } else if (instruction[0].equals("6")) {
-                int ans = arr.isEmpty() ? 1 : 0;
-                System.out.println(ans);
-            } else if (instruction[0].equals("7")) {
-                int ans = arr.size() != 0 ? arr.peek() : -1;
-                System.out.println(ans);
-            } else if (instruction[0].equals("8")) {
-                int ans = arr.size() != 0 ? arr.peekLast() : -1;
-                System.out.println(ans);
-            }
+            arr.add(Integer.parseInt(stringTokenizer.nextToken()));
         }
+
+        System.out.println(arr.size());
     }
 }
