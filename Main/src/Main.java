a13eb94b1c5a08cf4f751ac676bb7d49ea58179d
Yi-Yongmin <lym8467@naver.com>
2024-12-31 22:26:05 +0900

    baekjoon24479

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,8 @@
 import java.util.*;
 import java.io.*;
 
-public class App {
+public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 정점의 수 N (5 ≤ N ≤ 100,000), 간선의 수 M (1 ≤ M ≤ 200,000), 시작 정점 R (1 ≤ R ≤ N)이
@@ -18,6 +19,8 @@ public class App {
             graph[i] = new Node();
             graph[i].idx = i;
         }
+
+        HashSet<String> edges = new HashSet<>();
         for (int i = 0; i < M; i++) {
             str = br.readLine();
             stringTokenizer = new StringTokenizer(str, " ");
@@ -27,30 +30,37 @@ public class App {
             Node n2 = graph[second];
 
             // 만약 n1의 인접노드에 n2가 포함되어있지 않았다면,
-            if (!n1.adjacent.contains(n2))
+            if (!edges.contains(first + "-" + second) && !edges.contains(second + "-" + first)) {
                 graph[first].adjacent.add(graph[second]);
-            if (!n2.adjacent.contains(n1))
                 graph[second].adjacent.add(graph[first]);
+                edges.add(first + "-" + second);
+                edges.add(second + "-" + first);
+            }
         }
 
         // 오름차순으로 정렬
         for (int i = 1; i <= N; i++) {
-            graph[i].adjacent.sort((n1, n2) -> n1.idx - n2.idx);
+            graph[i].adjacent.sort((n1, n2) -> n2.idx - n1.idx);
         }
 
         // 방문 순서를 출력할 때 쓰일 변수 선언
         int cnt = 1;
         // 맨 처음에는 R부터 시작
-        graph[R].visited = cnt++;
         stack.push(graph[R]);
         // 근데 인접 노드는 낮은 숫자부터 오름차순으로 방문,,,?
         while (!stack.isEmpty()) {
             Node p = stack.pop();
-            for (Node n : p.adjacent) {
-                if (n.visited == 0) {
-                    n.visited = cnt++;
-                    stack.push(n);
+            if (p.visited <= 0) {
+                for (Node n : p.adjacent) {
+                    if (n.visited == 0) {
+                        n.visited = -1;
+                        stack.push(n);
+                    } else if (n.visited == -1) {
+                        stack.push(n);
+                    }
+
                 }
+                p.visited = cnt++;
             }
         }
 
@@ -58,17 +68,12 @@ public class App {
             System.out.println(graph[i].visited);
         }
     }
+
 }
 
-class Node implements Comparable<Node> {
+class Node {
     int idx;
     // 디폴트로 일단 방문 안한 것.
     int visited = 0;
     LinkedList<Node> adjacent = new LinkedList<>();
-
-    // 오름차순으로 출력하기 위한 compareTo 오버라이딩
-    @Override
-    public int compareTo(Node node) {
-        return this.idx - node.idx;
-    }
 }
