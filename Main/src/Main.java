10d3182b83b13538059848e85e72bc7573a9cf14
Yi-Yongmin <lym8467@naver.com>
2025-01-05 21:56:22 +0900

    baekjoon4948

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -5,40 +5,37 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        // StringTokenizer stringTokenizer = new StringTokenizer(str, " ");
-        // int N = Integer.parseInt(stringTokenizer.nextToken());
-
-        // N번
-        for (int i = 0; i < N; i++) {
-            long input = Long.parseLong(br.readLine());
-            if (input == 0 || input == 1) {
-                System.out.println(2);
-                continue;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0)
+                break;
+            int cnt = 0;
+            // n 초과 , 2n 이하인 소수들의 갯수
+            for (int i = n + 1; i < 2 * n + 1; i++) {
+                if (isPrime(i))
+                    cnt++;
             }
-
-            int end = 0;
-            while (true) {
-                // max까지만 돌리기
-                int max = (int) Math.sqrt(input); // 얘가 문제였다. -> 계속 업데이트 시켜줘야 함
-
-                for (int j = 2; j <= max; j++) {
-                    // 하나라도 약수가 있다면 , 바로 break 걸고 다음 수로
-                    if (input % j == 0) {
-                        end = 1;
-                        break;
-                    }
-                }
-                if (end == 1) { // 1이면 input 1증가, 0 max까지 약수가 없었으므로 소수 판단.
-                    end = 0;
-                    input++;
-                } else {
-                    System.out.println(input);
-                    break;
-                }
-            }
+            bw.write(cnt + "\n");
+            bw.flush();
         }
+    }
+
+    static boolean isPrime(int N) {
+        if (N <= 1)
+            return false;
+        if (N <= 3)
+            return true;
+        if (N % 2 == 0 || N % 3 == 0)
+            return false;
+        int until = (int) Math.sqrt(N);
+        // 소수의 배수면 그것 또한 탈락
+        for (int i = 5; i <= until; i += 6) {
+            if (N % i == 0 || N % (i + 2) == 0)
+                return false;
+        }
+        // 앞에꺼 다 통과하면 소수맞다.
+        return true;
     }
 
 }
